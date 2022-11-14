7c0a40d7a8dad0b2a8335380c57cf56eedb6546b
Zion Ng <zion@intzone.com>
2022-11-14 12:48:43 +0800

    Do not use dependencies for logging fatal errors

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Entrypoint for application
+ * Entrypoint for entire application
  */
 
 use App\ApiResponse;
@@ -14,21 +14,35 @@ chdir(dirname(__DIR__));
 // Set additional environment variables before loading PHP files
 putenv('GETMAIL_VERSION=' . trim(file_get_contents('VERSION.txt') ?: 'no-version'));
 
-// Composer autoloading
-require 'vendor/autoload.php';
-
-// Handle fatal errors - should not depend on any dependencies as much as possible
+// Set handler for fatal errors before loading PHP files - should not depend on any dependencies as much as possible
 register_shutdown_function(function () {
     $error = error_get_last();
     if (!$error || $error['type'] !== E_ERROR) { // skip if no error or not fatal error
         return;
     }
 
-    $response = new ApiResponse(500, $error['message']);
-    $response->send();
+    // Output error details to console only
+    // Logger class not used to format message/timestamp as fatal error may have been due to it
+    $fileHandle = fopen('php://stdout', 'w');
+    $utcDate = new DateTime('now', new DateTimeZone('UTC'));
+    fwrite(
+        $fileHandle,
+        '[' . $utcDate->format('Y-m-d\TH:i:s.up') . '] [FATAL] ' . json_encode($error) // follow format in Logger class
+    );
+    fclose($fileHandle);
+
+    // Do not show error details to client - ApiResponse class not used as fatal error may have been due to it
+    http_response_code(500);
+    header('Content-Type: text/html');
+    echo 'Fatal error occurred.';
+    exit;
 });
 
+// Load PHP files and run
 try {
+    // Composer autoloading
+    require 'vendor/autoload.php';
+
     // Init config and logger
     Config::init();
     Logger::init();
@@ -39,6 +53,6 @@ try {
 } catch (Throwable $t) {
     Logger::errorLog($t);
 
-    $response = new ApiResponse(500, $t->getMessage());
+    $response = new ApiResponse(500, 'An error occurred.'); // do not show error details to client
     $response->send();
 }
